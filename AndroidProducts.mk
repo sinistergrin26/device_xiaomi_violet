d763e552a95d8076470f8098ccc3f0e51fc9361a
sinistergrin26 <115425355+sinistergrin26@users.noreply.github.com>
2023-03-07 11:07:34 +0530

    Initial Tree for Calyx

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2020 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/calyx_violet.mk
+
+COMMON_LUNCH_CHOICES := \
+    calyx_violet-eng \
+    calyx_violet-userdebug \
+    calyx_violet-user
---
+++ calyx_violet.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2020 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from violet device
+$(call inherit-product, device/xiaomi/violet/device.mk)
+
+# Inherit some common cdDroid stuff.
+$(call inherit-product, vendor/calyx/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := calyx_violet
+PRODUCT_DEVICE := violet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 7 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="violet"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
